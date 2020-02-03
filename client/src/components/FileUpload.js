809351e86716c7dc68c4edf534577ed653d98b0d
Karol <karol.vogelgezang@gmail.com>
2020-02-03 21:06:46 +0100

    koniec

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,10 +1,14 @@
 import React, { Fragment, useState } from 'react';
+import Message from "./Message";
+import Progress from "./Progress";
 import axios from 'axios';
 
 const FileUpload = () => {
     const [file, setFile] = useState('');
     const [filename, setFilename] = useState('Choose file');
-    // const [uploadedFile, setUploadedFile] = useState({});
+    const [uploadedFile, setUploadedFile] = useState({});
+    const [message, setMessage] = useState('');
+    const [uploadPercentage, setUploadPercentage] = useState(0);
 
     const onChange = e => {
         setFile(e.target.files[0]);
@@ -23,19 +27,39 @@ const FileUpload = () => {
             const res = await axios.post('/upload', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
-                }
+                },
+                onUploadProgress: progressEvent => {
+                    setUploadPercentage(
+                        parseInt(
+                            Math.round((progressEvent.loaded*100)/progressEvent.total)
+                        )
+                    );
+
+
+                    // Clear percentage
+                    setTimeout(() => setUploadPercentage(0),10000);
+                },
             });
 
-            // const { fileName, filePath } = res.data;
-            //
-            // setUploadedFile({ fileName, filePath });
+            // Pull out fileName and filepath from res.data
+            const { fileName, filePath } = res.data;
+
+            // To nam stworzy this.state.uploadedFile.fileName oraz this.state.uploadedFile.filePath
+            setUploadedFile({ fileName, filePath });
+
+            setMessage('File Uploaded');
         } catch(err) {
-            console.log(err);
+            if (err.response.status === 500) {
+                setMessage('There was a problem with the server');
+            } else {
+                setMessage(err.response.data.msg);
+            }
         }
     };
 
     return (
         <Fragment>
+            {message ? <Message msg={message}/> : null}
             <form onSubmit={onSubmit}>
                 <div className="custom-file mb-4">
                     <input
@@ -51,12 +75,23 @@ const FileUpload = () => {
                         {filename}
                     </label>
                 </div>
+                <Progress percentage={uploadPercentage}/>
                 <input
                     type="submit"
                     value="Upload"
-                    className="btn btn-primary btn-block"
+                    className="btn btn-primary btn-block mt-4"
                 />
             </form>
+            {uploadedFile ?
+                <div className="row mt-5">
+                    <div className="col-md-6 m-auto">
+                        <h3 className="text-center">
+                            { uploadedFile.fileName }
+                        </h3>
+                        <img style={{ width: '100%'}} src={ uploadedFile.filePath } alt=""/>
+                    </div>
+                </div> : null
+            }
         </Fragment>
     )
 };
